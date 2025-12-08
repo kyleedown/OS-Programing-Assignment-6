e2647e05514f2c78c5af3ff1d09c3f113d8e6157
Haley Kothari <144263043+haleyskomet@users.noreply.github.com>
2025-12-07 18:08:51 -0800

    Updated
    I added a new one just in case, so that if we have to revert back, we can. I still need to add to the main but I think I have all the definitions and functions in there

--- vmmgr.c
+++ vmmgr.c
@@ -1,11 +1,35 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
+#include <string.h>
 #include <errno.h>
+#include <stdint.h>
+
+
+#define PAGE_COUNT 256
+#define FRAME_SIZE 256
+#define TLB_SIZE 16
+#define PHYSICAL_FRAMES 256
 
 typedef struct {
     uint8_t page;
     uint8_t offset;
 } PhysicalAddress;
+
+typedef struct {
+    int page;
+    int frame;
+    int lastUsed;
+} TLBEntry;
+
+TLBEntry tlb[TLB_SIZE];
+int pageTable[PAGE_COUNT];
+signed char physicalMemory[PHYSICAL_FRAMES][FRAME_SIZE];
+int frameToPage[PHYSICAL_FRAMES];
+int nextFreeFrame = 0;
+int pageFaults = 0;
+int tlbHits = 0;
+int totalAddresses = 0;
+int timeCounter = 0;
 
 PhysicalAddress convertAddress(uint32_t logicalAddress){
     PhysicalAddress address;
@@ -14,6 +38,52 @@ PhysicalAddress convertAddress(uint32_t logicalAddress){
     address.offset = masked & 0xFF;
     return address;
 
+}
+
+int tlbLookup(int page) {
+    for (int i = 0; i < TLB_SIZE; i++) {
+        if (tlb[i].page == page) {
+            tlb[i].lastUsed = timeCounter++;
+            tlbHits++;
+        return tlb[i].frame;
+        }
+    }
+    return -1;
+}
+
+void tlbInsert(int page, int frame) {
+    for (int i = 0; i < TLB_SIZE; i++) {
+        if (tlb[i].page == -1) {
+            tlb[i].page = page;
+            tlb[i].frame = frame;
+            tlb[i].lastUsed = timeCounter++;
+        return;
+        }
+    }
+    int lru = 0;
+    for (int i = 1; i < TLB_SIZE; i++) {
+        if (tlb[i].lastUsed < tlb[lru].lastUsed) lru = i;
+    }
+    tlb[lru].page = page;
+    tlb[lru].frame = frame;
+    tlb[lru].lastUsed = timeCounter++;
+}
+
+int loadPage(int page, FILE *backing) {
+    pageFaults++;
+    int frame = nextFreeFrame;
+    nextFreeFrame++;
+
+
+    fseek(backing, page * FRAME_SIZE, SEEK_SET);
+    fread(physicalMemory[frame], 1, FRAME_SIZE, backing);
+
+
+    pageTable[page] = frame;
+    frameToPage[frame] = page;
+
+
+    return frame;
 }
 
 int main(int argc, char *argv[]){
@@ -25,10 +95,10 @@ int main(int argc, char *argv[]){
 
     FILE *fp = NULL;
     char line[10];
-    fp = fopen(argv[1],"r");
+    fp = fopen(argv[1], "r");
 
 
-    if (fp == NULL){
+    if(fp == NULL) {
         fprintf(stderr, "Error opening file %s: ", argv[1]);
         perror("");
         return -1;
@@ -38,16 +108,14 @@ int main(int argc, char *argv[]){
 
     int physicalAddressPage,physicalAddressOffset;
 
-    while(fgets(line,sizeof(line),fp)){
+    while(fgets(line,sizeof(line),fp)) {
         uint32_t logical = atoi(line);
         printf("%d: ", logical);
         PhysicalAddress pa = convertAddress(logical);
         printf("%d, %d\n", pa.page,pa.offset);
-
     }
 
-
     fclose(fp);
-    return 0;
+    return 0; 
 
 }
